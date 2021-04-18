f3b954bd85eec8063bcb136e2598c21b1256a64a
AMRITPAL SINGH <saffi1999@icloud.com>
2021-04-17 20:09:18 -0400

    added firebase comand and API

---
+++ Ffirebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDZqs3v3S8ekj8Nzx9JfXxbemWy_r_pQB4",
+    authDomain: "ecomerce-store-dd20d.firebaseapp.com",
+    projectId: "ecomerce-store-dd20d",
+    storageBucket: "ecomerce-store-dd20d.appspot.com",
+    messagingSenderId: "789714804113",
+    appId: "1:789714804113:web:fcaffdcb2f82a35c228e30",
+    measurementId: "G-8CKW6Q1Q4B"
+  };
